013d6a92c92f96486d39f4470be6d20f857b4fcd
lawrenceChege <lawrencechege.s@gmail.com>
2018-05-29 15:46:55 +0300

    [feature #157930005] Add blank methods for signin test cases

--- tests/test_users.py
+++ tests/test_users.py
@@ -20,4 +20,21 @@ class TestUsersTestCase(BaseTestCase):
 
     def test_users_signup(self):
         pass
-        
+    
+    def test_users_signin(self):
+        pass
+
+    def test_users_signin_wrong_email(self):
+        pass
+    
+    def test_users_signin_wrong_password(self):
+        pass
+
+    def test_users_signin_empty_email(self):
+        pass
+
+    def test_users_signin_empty_password(self):
+        pass
+
+    def test_users_signin_not_registered_email(self):
+        pass
