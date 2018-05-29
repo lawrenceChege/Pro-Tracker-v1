1351672aa0824573b4d29c9bff2b2516b5dbd2ed
lawrenceChege <lawrencechege.s@gmail.com>
2018-05-29 14:14:29 +0300

    m [chore] setup the neccesary files and folders

---
+++ tests/base.py
@@ -0,0 +1,9 @@
+""" This is the base class for all the tests"""
+
+from unittest import TestCase
+
+import os
+import json
+
+class BaseTestCase(TestCase):
+    """ set up configurations for the test environment"""
