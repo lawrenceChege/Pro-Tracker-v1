a57b1e600c637f4b6bb26c76da8d8e56061fad4b
lawrenceChege <lawrencechege.s@gmail.com>
2018-05-29 15:41:13 +0300

    [feature #157930005] Add class and methods for user signup tests

---
+++ tests/test_users.py
@@ -0,0 +1,23 @@
+"""Tests for users"""
+
+from base import BaseTestCase
+
+import json
+
+class TestUsersTestCase(BaseTestCase):
+    """Tests for user authentication and authorization"""
+    def test_users_signup_empty_firstname(self):
+        pass
+
+    def test_users_signup_empty_lastname(self):
+        pass
+
+    def test_users_signup_empty_email(self):
+        pass
+
+    def test_users_signup_empty_password(self):
+        pass
+
+    def test_users_signup(self):
+        pass
+        
