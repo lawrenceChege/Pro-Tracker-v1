25f93c9cdb42b8b7a057d5b55d687d46c118bcf6
lawrenceChege <lawrencechege.s@gmail.com>
2018-05-29 16:29:20 +0300

    [feature

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,10 +4,17 @@ from base import BaseTestCase
 
 import json
 
+
 class TestUsersTestCase(BaseTestCase):
     """Tests for user authentication and authorization"""
+
     def test_users_signup_empty_firstname(self):
-        pass
+        response = self.app.post('/api/v1/auth/register',
+        data=json.dumps(
+            dict(username="", email="jim@gamil.com", password="12345")),
+        headers={'content-type': "application/json"})
+        response_msg = json.loads(response.data.decode())
+        self.assertIn("Username is required",response_msg["message"])
 
     def test_users_signup_empty_lastname(self):
         pass
