de797c1619235290022a430ec147100967e5c27b
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-06 09:43:51 +0300

    [chore 158093383] connect db

--- app/models.py
+++ app/models.py
@@ -2,34 +2,36 @@ import psycopg2
 
 #Try connecting to the database
 try:
-    con = psycopg2.connect("dbname='protrackerdb' user='lawrence' host='localhost' password='       '")
+    conn = psycopg2.connect(" dbname='protrackerdb' user='postgres' host='localhost' password='       '")
 except:
     print ("I am unable to connect to the database")
 
 
-cur = con.cursor()
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, username varchar, email varchar, password varchar)")
 
-cur.execute("CREATE TABLE User(id serial PRIMARY KEY, username varchar, email varchar, password varchar)")
+namedict = {"username": "Joshua", "email": "Drake@gmail.com", "password": "something"}
+            # {"username": "Steven", "email": "stephen@gmail.com",
+            #     "password": "somethingnew"},
+            # {"username": "David", "email": "devi@gmail.com", "password": "somethingelse"})
 
-namedict = ({"username": "Joshua", "email": "Drake@gmail.com", "password": "something"},
-            {"username": "Steven", "email": "stephen@gmail.com",
-                "password": "somethingnew"},
-            {"username": "David", "email": "devi@gmail.com", "password": "somethingelse"})
 
-cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-cur.executemany(
-    """INSERT INTO User(username,email, password) VALUES (%(username)s, %(email)s, %(password)s)""", namedict)
-con.commit()
+cur.execute(
+    """INSERT INTO users (username,email, password) VALUES (%s, %s, %s)""", (namedict["username"], namedict["email"], namedict["password"]))
+
+
 try:
-    cur.execute("""SELECT * from User""")
+    cur.execute("""SELECT * from users""")
+    items = cur.fetchall()
+    print (items)
 except:
     print ("I can't select from user")
 
-con.set_isolation_level(0)
-try:
-    cur.execute("""DROP DATABASE protraxkerdb""")
-except:
-    print ("I can't drop our database!")
-
+# conn.set_isolation_level(1)
+# try:
+#     cur.execute("""DROP DATABASE protrackerdb""")
+# except:
+#     print ("I can't drop our database!")
+conn.commit()
 cur.close()
-con.close()
+conn.close()
