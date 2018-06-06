8145baa79a9ce57fdb5bb9fde42bc3f19ce7fa44
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-06 15:24:31 +0300

    update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,12 @@
-from flask import Flask
+from flask_api import FlaskAPI
 
-app = Flask(__name__)
+# local import
+from instance.config import app_config
+
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    return app
