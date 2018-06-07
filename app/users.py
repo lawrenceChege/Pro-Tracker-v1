893d4533c2015ffce92e3e481b640e0b0ce02e0e
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-07 15:16:14 +0300

    [bug] validation of inpits

--- app/users.py
+++ app/users.py
@@ -26,13 +26,37 @@ class User(Resource):
     def post(self):
         """This class creates a user"""
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('username', type = str, required = True,
-        help = 'Username is required!', location = 'json')
-        self.reqparse.add_argument('email', type = str,required = True,
-        help = "Email is required!", location = 'json')
-        self.reqparse.add_argument('password', type = str, required = True,
-        help = "Passord is required!", location = 'json')
+        
+        self.reqparse.add_argument("username",
+                                    required=True,
+                                    help='Username is required!',
+                                    location='json')
         args =  self.reqparse.parse_args()
+        self.reqparse.add_argument(args["username"],
+                                    required=True,
+                                    help='Username is required and should be a valid string!',
+                                    location='json')
+        
+        self.reqparse.add_argument('email',
+                                    type = str,
+                                    required = True,
+                                    help = "Email is required!",
+                                    location = 'json')
+        args =  self.reqparse.parse_args()
+        self.reqparse.add_argument(args["email"],
+                                    required=True,
+                                    help='Email is required!',
+                                    location='json')
+        self.reqparse.add_argument('password',
+                                    type = str,
+                                    required = True,
+                                    help = "Passord is required!",
+                                    location = 'json')
+        args =  self.reqparse.parse_args()
+        self.reqparse.add_argument(args["password"],
+                                    required=True,
+                                    help='Password is required!',
+                                    location='json')
         username, email, password = args["username"], args["email"], args["password"]
         data = {
             "username" : username,
@@ -44,7 +68,19 @@ class User(Resource):
 class User_login(Resource):
     """This user logs in the user"""
     def post(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str, required = True,
+        help ="Username is required!")
+        self.reqparse.add_argument('password', type = str, required = True,
+        help = "Passord is required!", location = 'json')
+        args =  self.reqparse.parse_args()
+        username, password = args["username"], args["password"]
+        data ={
+            "username":username,
+            "password":password
+        }
+        return data
 
 
-api.add_resource(User_login, '/api/v1/auth/')
-api.add_resource(User, '/api/v1/auth/')
+api.add_resource(User_login, '/api/v1/auth/login')
+api.add_resource(User, '/api/v1/auth/signup')
--- tests/test_user.py
+++ tests/test_user.py
@@ -7,70 +7,67 @@ import unittest
 import psycopg2
 import json
 
-class TestRequestTestCase(BaseTestCase):
+class TestUserTestCase(BaseTestCase):
     """ Test for normal user"""
     def setUp(self):
         """class initializations"""
-        self.person_no_username = self.person_no_username
-        # self.username = username
-        # self.email = email
-        # self.password = pbkdf2_sha256.hash("password")
-        # self.app = app.test_client()
-
+        pass
+       
     def test_user_signup(self):
         """Test for user signup"""
         #no username
-        response = self.app.post('/api/v1/auth/',
+        response = self.app.post('/api/v1/auth/signup',
                                  data=json.dumps(self.person_no_username),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
+        print(response)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Username is required')
 
         #no email
-        response = self.app.post('api/v1/auth/',
+        response = self.app.post('api/v1/auth/signup',
                                  data=json.dumps(self.person_no_email),
                                  headers={'content-type': "application/json"})
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Email is required!')
 
         #invalid email
-        response = self.app.post('api/v1/auth/',
+        response = self.app.post('api/v1/auth/signup',
                                  data=json.dumps(self.person_invalid_email),
                                  headers={'content-type': "application/json"})
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'User created successfully!')
         #no password
-        response = self.app.post('api/v1/auth/',
+        response = self.app.post('api/v1/auth/signup',
                                  data=json.dumps(self.person_no_password),
                                  headers={'content-type': "application/json"})
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Passord is required!')
         #correct details
-        response = self.app.post('api/v1/auth/',
+        response = self.app.post('api/v1/auth/signup',
                                  data=json.dumps(self.person),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,201)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'User created successfully!')
         #existing user
-        response = self.app.post('api/v1/auth/',
+        response = self.app.post('api/v1/auth/signup',
                                  data=json.dumps(self.person_existing_user),
                                  headers={'content-type': "application/json"})
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'User already exists')
 
-    def test_login(self):
-        """Test for login"""
-        # response = self.app.post('/api/v1/requests/0', data=json.dumps(
-        #     self.request)
-        self.assertEqual(response.status_code, 201)
-        data = json.loads(response.get_data())
-        self.assertEqual(data['message'],'Request Added Successfully')
+    # def test_login(self):
+    #     """Test for login"""
+    #     response = self.app.post('/api/v1/requests/0', data=json.dumps(
+    #         self.request)
+    #     self.assertEqual(response.status_code, 201)
+    #     data = json.loads(response.get_data())
+    #     self.assertEqual(data['message'],'Request Added Successfully')
         #correct
         #incorrect
         #empty
