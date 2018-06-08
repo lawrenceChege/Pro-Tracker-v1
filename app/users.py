5f9acc2d57317bd1e402f15667be88b5f5691165
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-08 13:06:45 +0300

    user return user_id

--- app/users.py
+++ app/users.py
@@ -82,15 +82,17 @@ class User_login(Resource):
             cur.execute("""SELECT * FROM users""")
             result = cur.fetchall()
             if username in result and pbkdf2_sha256.verify(password, hash):
+                cur.execute("""SELECT user_id FROM users WHERE username = username """)
+                user_id = cur.fetchall()
                 return "User successfully logged in"
             else:
                 return "please check your credentials!"
         except:
             print ("I could not  select from user")
-        
+
         access_token = create_access_token(identity=username)
         token = str(access_token)
-        return (token), 201
+        return (token),user_id, 201
 class Get_user(Resource):
     """Gets user details"""
     def get(self, user_id):
