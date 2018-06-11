0f880542c8a343402ad6e2d0385e2e3801b8f223
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-11 13:24:45 +0300

    [fix]tests not found

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -15,34 +15,34 @@ class TestRequestsTestCase(BaseTestCase):
     def test_user_make_new_request(self):
         """Test for making new request"""
         #not json
-        response = self.app.post('/api/v1/requests/0', data=self.request)
+        response = self.app.post('/api/v2/requests/', data=self.request)
         self.assertEqual(response.status_code, 400)
         #correct request
-        response = self.app.post('/api/v1/requests/0', data=json.dumps(
+        response = self.app.post('/api/v2/requests/', data=json.dumps(
             self.request), headers={'content-type': "application/json"})
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.get_data())
         self.assertEqual(data['message'],'Request Added Successfully')
         #no category
-        response = self.app.post('/api/v1/requests/0', data=json.dumps(
+        response = self.app.post('/api/v2/requests/', data=json.dumps(
             self.request_no_category), headers={'content-type': "application/json"})
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.get_data())
         self.assertEqual(data['message'],'Please enter category as either repair or maintenanc')
         #no title
-        response = self.app.post('/api/v1/requests/0', data=json.dumps(
+        response = self.app.post('/api/v2/requests/', data=json.dumps(
             self.request_no_title), headers={'content-type': "application/json"})
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.get_data())
         self.assertEqual(data['message'],'Title should be a string')
         #no description
-        response = self.app.post('/api/v1/requests/0', data=json.dumps(
+        response = self.app.post('/api/v2/requests/', data=json.dumps(
             self.request_no_description), headers={'content-type': "application/json"})
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.get_data())
         self.assertEqual(data['message'],'Description is a string')
         #no frequency
-        response = self.app.post('/api/v1/requests/0', data=json.dumps(
+        response = self.app.post('/api/v2/requests/', data=json.dumps(
             self.request_no_frequency ), headers={'content-type': "application/json"})
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.get_data())
@@ -59,7 +59,7 @@ class TestRequestsTestCase(BaseTestCase):
 
     def test_user_view_a_users_requests(self):
         """Test for viewing a user's requests """
-        response = self.app.get('/api/v1/requests/')
+        response = self.app.get('/api/v2/requests/')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.get_data())
         self.assertEqual( data['message'] , "all requests found successfully")
@@ -68,7 +68,7 @@ class TestRequestsTestCase(BaseTestCase):
     def test_user_view_a_request(self):
         """Test for vieving a particular request"""
 
-        response_message = self.app.get('/api/v1/requests/0/0/')
+        response_message = self.app.get('/api/v2/requests/0/0/')
         self.assertEqual(response_message.status_code, 200)
         data = json.loads(response_message.get_data())
         self.assertEqual( data['message'] , "Request successfully retrieved")
@@ -96,7 +96,7 @@ class TestRequestsTestCase(BaseTestCase):
     def test_user_modify_a_request(self):
         """Test for modifying a request"""
         
-        response = self.app.put('/api/v1/requests/0/1/',
+        response = self.app.put('/api/v2/requests/3',
                                         data=json.dumps(
                                             dict(category="repair")),
                                         headers={'content-type': "application/json"})
@@ -107,7 +107,7 @@ class TestRequestsTestCase(BaseTestCase):
     def test_user_delete_a_request(self):
         """Test for deleting a request"""
 
-        response = self.app.delete('/api/v1/requests/0/2/')
+        response = self.app.delete('/api/v2/requests/3')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.get_data())
         self.assertEqual( data['message'] , "Request successfuly deleted")
--- tests/test_user.py
+++ tests/test_user.py
@@ -12,16 +12,16 @@ class TestUserTestCase(BaseTestCase):
     def test_user_signup(self):
         """Test for user signup"""
         #no username
-        response = self.app.post('/api/v1/auth/signup',
+        response = self.app.post('/api/v2/auth/signup',
                                  data=json.dumps(self.person_no_username),
                                  headers={'content-type': "application/json"})
-        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.status_code,400)
         print(response)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Username is required')
 
         #no email
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.person_no_email),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
@@ -29,28 +29,28 @@ class TestUserTestCase(BaseTestCase):
         self.assertEqual(dataman['message'],'Email is required!')
 
         #invalid email
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.person_invalid_email),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'User created successfully!')
         #no password
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.person_no_password),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Passord is required!')
         #correct details
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.person),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,201)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'User created successfully!')
         #existing user
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.person_existing_user),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
@@ -60,28 +60,28 @@ class TestUserTestCase(BaseTestCase):
     def test_login(self):
         """Test for login"""
         #nopassword
-        response = self.app.post('api/v1/auth/login',
+        response = self.app.post('api/v2/auth/login',
                                  data=json.dumps(self.no_password),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'Password is required')
         #no username
-        response = self.app.post('api/v1/auth/login',
+        response = self.app.post('api/v2/auth/login',
                                  data=json.dumps(self.no_username),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,400)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'password is required')
         #incorrect
-        response = self.app.post('api/v1/auth/login',
+        response = self.app.post('api/v2/auth/login',
                                  data=json.dumps(self.wrong_login),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,401)
         dataman = json.loads(response.get_data())
         self.assertEqual(dataman['message'],'please check your credential')
         #empty
-        response = self.app.post('api/v1/auth/signup',
+        response = self.app.post('api/v2/auth/signup',
                                  data=json.dumps(self.correct_login),
                                  headers={'content-type': "application/json"})
         self.assertEqual(response.status_code,200)
